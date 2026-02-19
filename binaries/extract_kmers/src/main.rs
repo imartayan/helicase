765c0f655e58cc6e02c69942bc072b749e2e49d1
Charles Paperman <charles.paperman@univ-lille.fr>
2026-02-19 21:54:12 +0100

    adding a binary for building kmer collection

--- binaries/extract_kmers/src/main.rs
+++ binaries/extract_kmers/src/main.rs
@@ -24,6 +24,17 @@ fn init_tracing(log_level: &str) {
 }
 
 #[derive(ClapParser, Debug)]
+#[derive(Debug, Clone, ValueEnum)]
+enum TypeLayout {
+    U8,
+    U16,
+    U32,
+    U64,
+    U128,
+}
+
+#[derive(Parser, Debug)]
+>>>>>>> 9dd698f (adding a binary for building kmer collection)
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Input file (FASTA/FASTQ)
@@ -31,6 +42,7 @@ struct Args {
     /// kmer size
     #[arg(short, long)]
     k: usize,
+<<<<<<< HEAD
     /// without deduplication
     #[arg(short = 'w', long)]
     dedup: bool,
@@ -122,4 +134,22 @@ fn main() {
     let args = Args::parse();
     init_tracing(&args.log_level);
     dispatch_k!(args.k, |K, T| main_dispatched::<K, T>(&args))
+=======
+    /// type layout
+    #[arg(short = 'c', long)]
+    t: TypeLayout,
+}
+fn main() {
+    let args = Args::parse();
+    let path = &args.input;
+    let start = Instant::now();
+    let data = read(path).expect("Cannot open file");
+    let elapsed = start.elapsed().as_secs_f64();
+    println!("Loading the data in {}s", elapsed);
+    let start = Instant::now();
+    let kmers = kmer_from_fastx_slice::<63, u64>(&data).expect("invalid data");
+    let elapsed = start.elapsed().as_secs_f64();
+    println!("Computing kmers in {}s", elapsed);
+    println!("{}", kmers.len());
+>>>>>>> 9dd698f (adding a binary for building kmer collection)
 }
--- helicase/src/kmer/mod.rs
+++ helicase/src/kmer/mod.rs
@@ -3,4 +3,4 @@ mod bitstring;
 pub mod kmer_layouts;
 pub mod extract;
 pub use bitstring::{BitStorage, BitString};
-pub use kmer_layouts::{Mer, MerChunk};
+pub use kmer_layouts::{Kmer, Mer};

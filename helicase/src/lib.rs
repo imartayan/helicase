0765985034050922b75d77bab3117b4c9fc383b1
Charles Paperman <charles.paperman@univ-lille.fr>
2026-03-12 11:17:03 +0100

    add no-pdep feature to bench

--- helicase/src/lib.rs
+++ helicase/src/lib.rs
@@ -43,3 +43,7 @@ pub(crate) mod simd {
     mod fallback;
     pub use fallback::*;
 }
+#[cfg(feature = "no-pdep")]
+pub const PDEP_ACTIVATE: bool = true;
+#[cfg(not(feature = "no-pdep"))]
+pub const PDEP_ACTIVATE: bool = false;
